f0a8b80569aba46b6dad4ab84a958c3e7a4b255b
cstyczen27-sudo <Cstyczen27@adrian.edu>
2026-04-24 14:56:52 -0400

    first code added for admin btn functionality

--- Javascript/Admin.js
+++ Javascript/Admin.js
@@ -1,1 +1,9 @@
 console.log("The Js file is connected")
+
+let adminMode = false;
+
+document.getElementById("barbtn").addEventListener("click", function toggleAdmin() 
+    {adminMode = !adminMode;
+    console.log(adminMode);
+});
+console.log(adminMode);
